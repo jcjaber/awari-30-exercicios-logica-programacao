846b3e52782459fa02ca0b73a3a87b47fc352214
Cassio Jaber <jcjaber@gmail.com>
2022-02-17 15:24:34 -0300

    resolução exercício 04

---
+++ exercicio-04/scripts.js
@@ -0,0 +1,12 @@
+let n = prompt("Insira um valor: ")
+let sum = 0
+
+console.log("S = 0")
+
+for(divisor = 1; divisor <= n; divisor++) {
+  
+  sum += 1 / divisor
+
+  console.log("+ 1/", divisor, " = ", sum)
+  
+}
